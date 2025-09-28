bb7a29ac05b529ceb1875e18d74ab46172ac867f
Mike Monaco <mamonaco1973@gmail.com>
2025-09-28 14:04:35 -0400

    next phase of the build

--- api_setup.sh
+++ api_setup.sh
@@ -20,6 +20,14 @@ gcloud services enable compute.googleapis.com
 gcloud services enable firestore.googleapis.com
 gcloud services enable cloudresourcemanager.googleapis.com
 gcloud services enable storage.googleapis.com
-gcloud services enable secretmanager.googleapis.com
-gcloud services enable file.googleapis.com
+gcloud services enable apigateway.googleapis.com
+gcloud services enable servicemanagement.googleapis.com
+gcloud services enable servicecontrol.googleapis.com
+gcloud services enable cloudfunctions.googleapis.com
+gcloud services enable logging.googleapis.com
+gcloud services enable run.googleapis.com
+gcloud services enable artifactregistry.googleapis.com
+gcloud services enable pubsub.googleapis.com
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable iam.googleapis.com
 
